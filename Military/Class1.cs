6dc4aec824437081bf95ead4e78d8825b9999aa5
NeiroSet <Scorpion_run_142@mail.ru>
2024-05-16 01:54:29 +0500

    Добавьте файлы проекта.

---
+++ Military/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Military
+{
+    public class MilitaryUnit
+    {
+        public MilitaryUnit()
+        {
+            _part = new PartCommander();
+            _platoon = new PlatoonСommander();
+        }
+
+        public void Say()
+        {
+            _part.Say();
+            _platoon.Say();
+        }
+
+        private PartCommander _part;
+        private PlatoonСommander _platoon;
+    }
+
+    class PlatoonСommander
+    {
+        public void Say()
+        {
+            Console.WriteLine("Командир взвода Пупкин по Вашему приказанию прибыл!");
+        }
+    }
+
+    class PartCommander
+    {
+        public void Say()
+        {
+            Console.WriteLine("Командир отделения Дрищук по Вашему приказанию прибыл!");
+        }
+    }
+
+}
